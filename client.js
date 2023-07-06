85c54888a0c5732843fc5b548923f4a8e3c42c98
jasmine hilton <jasminehilton1@live.com>
2023-07-06 21:39:17 +0000

    final commit for snek.

--- client.js
+++ client.js
@@ -1,12 +1,12 @@
-const net = require("net");
-const { IP, PORT } = require("./constants");
+// const net = require("net");
+const { IP, PORT, conn } = require("./constants");
 // establishes a connection with the game server
 
 const connect = function () {
-  const conn = net.createConnection({
-    // host: "165.227.47.243",
-    // port: 50541,
-  });
+  // const conn = net.createConnection({
+  //   // host: "165.227.47.243",
+  //   // port: 50541,
+  // });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
--- constants.js
+++ constants.js
@@ -1,7 +1,15 @@
+const net = require("net");
+
 const IP = "165.227.47.243";
 const PORT = 50541;
+
+const conn = net.createConnection({
+  host: IP,
+  port: PORT,
+});
 
 module.exports = {
   IP,
   PORT,
+  conn,
 };
